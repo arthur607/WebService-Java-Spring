2fe90570d486ae970f0cb36672de95d364bf494a
arthur607 <arthurrodriguesg1@gmail.com>
2021-08-14 19:28:50 -0300

    start project

---
+++ src/test/java/com/arthur/springWebService/SpringWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthur.springWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
