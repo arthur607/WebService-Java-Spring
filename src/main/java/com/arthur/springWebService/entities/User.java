2396fd92a5ac4b0b8a35dc3e93a747064c5e750e
arthur607 <arthurrodriguesg1@gmail.com>
2021-08-14 20:33:22 -0300

    jpa and h2 database

--- src/main/java/com/arthur/springWebService/entities/User.java
+++ src/main/java/com/arthur/springWebService/entities/User.java
@@ -4,13 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import sun.security.util.Password;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
 @Data
 @AllArgsConstructor
-
+@Entity                                                     //mapping entity in repository
 public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)    //id auto increment
     private Long id;
     private String nome;
     private String email;
