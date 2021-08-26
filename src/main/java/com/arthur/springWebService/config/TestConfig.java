c43230c93a80843dd26e21595596e3df0f13a64a
arthur607 <arthurrodriguesg1@gmail.com>
2021-08-25 21:05:40 -0300

    order config

--- src/main/java/com/arthur/springWebService/config/TestConfig.java
+++ src/main/java/com/arthur/springWebService/config/TestConfig.java
@@ -1,12 +1,15 @@
 package com.arthur.springWebService.config;
 
+import com.arthur.springWebService.entities.Order;
 import com.arthur.springWebService.entities.User;
+import com.arthur.springWebService.repositories.OrderRepository;
 import com.arthur.springWebService.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -17,6 +20,8 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired                                      //dependency of server, weak dependency
     private UserRepository userRepository;
+    @Autowired
+    private OrderRepository orderRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -24,8 +29,11 @@ public class TestConfig implements CommandLineRunner {
         list.add(new User(null,"Arthur","arthur@garcia.com","12345"));
         list.add(new User(null,"Pedro","pedro@luiz.com","54321"));
 
+        List<Order> listOrder = new ArrayList<>();
+        listOrder.add(new Order(null, Instant.now(),list.get(1)));
+
         userRepository.saveAll(Arrays.asList(list.get(0),list.get(1)));
-       // userRepository.saveAll(list);
+        orderRepository.saveAll(listOrder);
 
     }
 }
