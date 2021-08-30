a52127cd6098975660d255867c2782f3481efda8
arthur607 <arthurrodriguesg1@gmail.com>
2021-08-29 21:25:50 -0300

    add entity Product

--- src/main/java/com/arthur/springWebService/controller/ProductController.java
+++ src/main/java/com/arthur/springWebService/controller/ProductController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/categories")
+@RequestMapping(value = "/products")
 public class ProductController {
 
     @Autowired
